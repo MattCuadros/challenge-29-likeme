64ac68fc2bf8aa616c8c532dd6c9c5fc5c2f729a
Matt Cuadros <mcuadrose@gmail.com>
2023-06-02 00:25:10 -0400

    Controller added

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,64 +1,12 @@
 import { Router } from "express";
-import { addPost, getPost, getPosts } from "../db/query.js";
+import { indexController } from "../controllers/index.controller.js";
 
 const router = Router();
 
-router.get("/posts", async (req, res) => {
-  try {
-    const result = await getPosts();
-    return res.json({
-      ok: true,
-      message: "Lista de Posts",
-      result,
-    });
-  } catch (error) {
-    console.log(error);
-    return res
-      .status(code)
-      .json({ ok: false, code: 404, message: "No se encuentra", result });
-  }
-});
+router.get("/posts", indexController.getAllPosts);
 
-router.post("/posts", async (req, res) => {
-  try {
-    const { titulo, url, descripcion } = req.body;
-    const result = await addPost({ titulo, url, descripcion });
-    return res.json({
-      ok: true,
-      message: "Nuevo Post añadido",
-      result,
-    });
-  } catch (error) {
-    console.log(error);
-    return res
-      .status(code)
-      .json({ ok: false, code: 404, message: "No se encuentra", result });
-  }
-});
+router.post("/posts", indexController.makePost);
 
-router.get("/posts/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const result = await getPost(id);
-    return res.json({
-      ok: true,
-      message: "Post encontrado",
-      result,
-    });
-  } catch (error) {
-    console.log(error);
-    if (error.code === "22P02") {
-      return res
-        .status(400)
-        .json({ ok: false, message: "Formato no válido en el parámetro" });
-    }
-    if (error.code === "404") {
-      return res
-        .status(404)
-        .json({ ok: false, message: "No existe ese registro" });
-    }
-    return res.status(500).json({ ok: false, message: "Error de Servidor" });
-  }
-});
+router.get("/posts/:id", indexController.getOnePost);
 
 export default router;
